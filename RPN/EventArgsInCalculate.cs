0eb5a3adc61894955d3978de609038a604be019c
Viktar <rusty.vik@gmail.com>
2024-01-14 22:34:18 +0300

    Rpn1 implement main functionality (#6)
    * Implemented basic functionality
    
    * Impemented basic logic, included events, using Chain of responsibility pattern. Added xUnit tests.
    
    * Fixed some issues, added Reset() method, implemented basic thread safety.
    
    * Added Resources files
    
    * Fixed issue.
    
    * Fixed issue.
    
    * The set of values passed via an event to the outside of the library has been expanded.

--- RPN/EventArgsInCalculate.cs
+++ RPN/EventArgsInCalculate.cs
@@ -10,21 +10,21 @@ public record EventArgsInCalculate
     /// <summary>
     /// Gets the instance of OperationInfo class. 
     /// </summary>
-    internal OperationInfo OperationInfo { get; }
+    public OperationInfo OperationInfo { get; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EventArgsInCalculate"/> class with a binary mathematical operator/function, two operands, and the triggering method.
     /// </summary>
-    internal EventArgsInCalculate(string mathOperator, double firstOperand, double secondOperand, [CallerMemberName] string triggerMethod = "")
+    internal EventArgsInCalculate(string mathOperator, double firstOperand, double secondOperand, double stepResult, [CallerMemberName] string triggerMethod = "")
     {
-        OperationInfo = new OperationInfo(mathOperator, firstOperand, secondOperand, triggerMethod);
+        OperationInfo = new OperationInfo(mathOperator, firstOperand, secondOperand, stepResult, triggerMethod);
     }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EventArgsInCalculate"/> class with a unary mathematical operand/function, a single operand, and the triggering method.
     /// </summary>
-    internal EventArgsInCalculate(string mathOperator, double firstOperand, [CallerMemberName] string triggerMethod = "")
+    internal EventArgsInCalculate(string mathOperator, double firstOperand, double stepResult, [CallerMemberName] string triggerMethod = "")
     {
-        OperationInfo = new OperationInfo(mathOperator, firstOperand, triggerMethod);
+        OperationInfo = new OperationInfo(mathOperator, firstOperand, stepResult, triggerMethod);
     }
 }
--- RPN/OperationInfo.cs
+++ RPN/OperationInfo.cs
@@ -6,27 +6,37 @@ namespace VV.Algorithm.RPN;
 /// Represents information about a mathematical operation or function and operands used in an expression. 
 /// Used in <see cref="EventArgsInCalculate"/>.
 /// </summary>
-internal class OperationInfo
+public sealed class OperationInfo
 {
+    /// <summary>
+    /// Defining flag, unary and binary operation. 
+    /// </summary>
+    public bool isUnary;
+    
     /// <summary>
     /// Gets the mathematical operator or function used in the expression.
     /// </summary>
-    internal string? MathOperator { get; }
+    public string? MathOperator { get; }
 
     /// <summary>
     /// Gets the first operand used in the expression.
     /// </summary>
-    internal double FirstOperand { get; }
+    public double FirstOperand { get; }
 
     /// <summary>
     /// Gets the second operand used in the expression.
     /// </summary>
-    internal double SecondOperand { get; }
+    public double SecondOperand { get; }
+
+    /// <summary>
+    /// Gets the result of one operation.
+    /// </summary>
+    public double StepResult { get;  }
 
     /// <summary>
     /// Gets the method that triggered the event.
     /// </summary>
-    internal string TriggerMethod { get; }
+    public string TriggerMethod { get; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OperationInfo"/> class for a binary operation.
@@ -34,13 +44,16 @@ internal class OperationInfo
     /// <param name="mathOperator">A binary math operation or function.</param>
     /// <param name="firstOperand">The first operand.</param>
     /// <param name="secondOperand">The second operand.</param>
+    /// <param name="stepResult">The result of one opertion.</param>
     /// <param name="triggerMethod">A method that triggered the event.</param>
-    internal OperationInfo(string? mathOperator, double firstOperand, double secondOperand, string triggerMethod)
+    internal OperationInfo(string? mathOperator, double firstOperand, double secondOperand, double stepResult, string triggerMethod)
     {
         MathOperator = mathOperator;
         FirstOperand = firstOperand;
         SecondOperand = secondOperand;
         TriggerMethod = triggerMethod;
+        StepResult = stepResult;
+        isUnary = false;
     }
 
     /// <summary>
@@ -48,11 +61,37 @@ internal class OperationInfo
     /// </summary>
     /// <param name="mathOperator">An unary math operation or function.</param>
     /// <param name="firstOperand">The first operand.</param>
+    /// <param name="stepResult">The result of one opertion.</param>
     /// <param name="triggerMethod">The second operand.</param>
-    internal OperationInfo(string mathOperator, double firstOperand, [CallerMemberName] string triggerMethod = "")
+    internal OperationInfo(string mathOperator, double firstOperand, double stepResult, [CallerMemberName] string triggerMethod = "")
     {
         MathOperator = mathOperator;
         FirstOperand = firstOperand;
         TriggerMethod = triggerMethod;
+        StepResult = stepResult;
+        isUnary = true;
     }
+
+    /// <summary>
+    /// Gets a string representation of the operation, including operand(s) and operator.
+    /// </summary>
+    /// <returns>A formatted string representing the operation.</returns>
+    public string GetOperationInfo()
+    {
+        if (!isUnary)
+        {
+            return $"{FirstOperand}{MathOperator}{SecondOperand} = ";
+        }
+        else
+        {
+            return $"{MathOperator}{FirstOperand} = ";
+        }
+    }
+    
+    /// <summary>
+    /// Returns a string representation of the result of calculating one operation in an expression.
+    /// </summary>
+    /// <returns>A string representation of the result of calculating one operation in an expression.</returns>
+    public string GetResultInfo()
+        => $"{StepResult}";
 }
